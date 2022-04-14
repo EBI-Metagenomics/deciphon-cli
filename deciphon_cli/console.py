f5e4c960d72e3c680cf5e0bd485d13054bd69cc9
Danilo Horta <danilo.horta@pm.me>
2022-04-14 12:47:30 +0100

    X-API-KEY

--- deciphon_cli/console.py
+++ deciphon_cli/console.py
@@ -10,31 +10,40 @@ from deciphon_cli.core import ScanPost, SeqPost
 
 run = typer.Typer()
 
-SCHED_API_URL = config("SCHED_API_URL", default="http://127.0.0.1:8000/api")
+API_HOST = config("host", default="127.0.0.1")
+API_PORT = config("port", default=49329)
+API_PREFIX = config("api_prefix", default="")
+API_KEY = config("api_key", default="change-me")
+
+API_URL = f"http://{API_HOST}:{API_PORT}{API_PREFIX}"
 
 
 class Headers:
     recv = {"Accept": "application/json"}
-    send = {"Content-Type": "application/json"}
-    both = {"Accept": "application/json", "Content-Type": "application/json"}
+    send = {"Content-Type": "application/json", "X-API-KEY": API_KEY}
+    both = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-API-KEY": API_KEY,
+    }
 
 
 @run.command()
 def hmm_list():
-    r = requests.get(f"{SCHED_API_URL}/hmms", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/hmms", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def hmm_rm(hmm_id: int):
-    r = requests.delete(f"{SCHED_API_URL}/hmms/{hmm_id}", headers=Headers.recv)
+    r = requests.delete(f"{API_URL}/hmms/{hmm_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def hmm_add(hmm_file: Path):
     r = requests.post(
-        f"{SCHED_API_URL}/hmms/",
+        f"{API_URL}/hmms/",
         headers=Headers.recv,
         files={
             "hmm_file": (
@@ -49,7 +58,7 @@ def hmm_add(hmm_file: Path):
 
 @run.command()
 def hmm_get(hmm_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/hmms/{hmm_id}", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/hmms/{hmm_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
@@ -57,49 +66,49 @@ def hmm_get(hmm_id: int = typer.Argument(...)):
 def db_get(
     db_id: int = typer.Argument(...),
 ):
-    r = requests.get(f"{SCHED_API_URL}/dbs/{db_id}", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/dbs/{db_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def db_list():
-    r = requests.get(f"{SCHED_API_URL}/dbs", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/dbs", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def db_rm(db_id: int):
-    r = requests.delete(f"{SCHED_API_URL}/dbs/{db_id}", headers=Headers.recv)
+    r = requests.delete(f"{API_URL}/dbs/{db_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def job_pend():
-    r = requests.get(f"{SCHED_API_URL}/jobs/next_pend", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/jobs/next_pend", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def job_list():
-    r = requests.get(f"{SCHED_API_URL}/jobs", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/jobs", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def job_get(job_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/jobs/{job_id}", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/jobs/{job_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def job_prod(job_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/jobs/{job_id}/prods", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/jobs/{job_id}/prods", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def job_rm(job_id: int):
-    r = requests.delete(f"{SCHED_API_URL}/jobs/{job_id}", headers=Headers.recv)
+    r = requests.delete(f"{API_URL}/jobs/{job_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
@@ -116,88 +125,88 @@ def scan_add(
             seq = SeqPost(name=item.id, data=item.sequence)
             scan.seqs.append(seq)
 
-    r = requests.post(f"{SCHED_API_URL}/scans/", headers=Headers.both, json=scan.dict())
+    r = requests.post(f"{API_URL}/scans/", headers=Headers.both, json=scan.dict())
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def scan_get(scan_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/scans/{scan_id}", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def scan_seq_list(scan_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/seqs", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/seqs", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def scan_list():
-    r = requests.get(f"{SCHED_API_URL}/scans", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/scans", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def scan_prod_list(scan_id: int = typer.Argument(...)):
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def scan_prod_gff(scan_id: int = typer.Argument(...)):
     headers = {"Accept": "text/plain"}
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods/gff", headers=headers)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods/gff", headers=headers)
     typer.echo(r.text, nl=False)
 
 
 @run.command()
 def scan_prod_path(scan_id: int = typer.Argument(...)):
     headers = {"Accept": "text/plain"}
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods/path", headers=headers)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods/path", headers=headers)
     typer.echo(r.text, nl=False)
 
 
 @run.command()
 def scan_prod_fragment(scan_id: int = typer.Argument(...)):
     headers = {"Accept": "text/plain"}
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods/fragment", headers=headers)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods/fragment", headers=headers)
     typer.echo(r.text, nl=False)
 
 
 @run.command()
 def scan_prod_amino(scan_id: int = typer.Argument(...)):
     headers = {"Accept": "text/plain"}
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods/amino", headers=headers)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods/amino", headers=headers)
     typer.echo(r.text, nl=False)
 
 
 @run.command()
 def scan_prod_codon(scan_id: int = typer.Argument(...)):
     headers = {"Accept": "text/plain"}
-    r = requests.get(f"{SCHED_API_URL}/scans/{scan_id}/prods/codon", headers=headers)
+    r = requests.get(f"{API_URL}/scans/{scan_id}/prods/codon", headers=headers)
     typer.echo(r.text, nl=False)
 
 
 @run.command()
 def prod_list():
-    r = requests.get(f"{SCHED_API_URL}/prods", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/prods", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def seq_list():
-    r = requests.get(f"{SCHED_API_URL}/seqs", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/seqs", headers=Headers.recv)
     typer.echo(json.dumps(r.json(), indent=2))
 
 
 @run.command()
 def sched_wipe():
-    r = requests.delete(f"{SCHED_API_URL}/sched/wipe", headers=Headers.recv)
+    r = requests.delete(f"{API_URL}/sched/wipe", headers=Headers.recv)
     typer.echo(r.text)
 
 
 @run.command()
 def sched_check_health():
-    r = requests.get(f"{SCHED_API_URL}/sched/check_health", headers=Headers.recv)
+    r = requests.get(f"{API_URL}/sched/check_health", headers=Headers.recv)
     typer.echo(r.text)
